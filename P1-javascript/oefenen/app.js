b45deebd7712e10b7a493c72535ca8fe6de3cbf0
haville <9020580@student.zadkine.nl>
2022-09-15 15:01:39 +0200

    opdracht 3.1

---
+++ P1-javascript/oefenen/app.js
@@ -0,0 +1,42 @@
+alert("hello world")
+
+let name = "chaville"
+
+const animal = "horse";
+
+//laat de variable naam in een alert zien op het schrem
+
+const myName = ','
+
+let myAge = 18;
+
+let constante = true
+
+let passed; 
+
+const diploma = true;
+
+let num1 = 70;
+let num = 69;
+
+let anwser = num1 + num2;
+console.log(anwser)
+
+let num3 = 70;
+let num4 = 69;
+
+let anwser1 = num1 - num2;
+console.log(anwser1)
+
+let anwser2 = num5 * num6;
+
+let num5 = 70;
+let num6 = 69;
+
+let anwser3 = num5 - num6;
+console.log(anwser3)
+
+
+
+
+ 
