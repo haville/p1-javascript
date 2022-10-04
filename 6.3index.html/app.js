ee762e20460700e441e96dbb546dcab696e2d5b7
haville <9020580@student.zadkine.nl>
2022-10-04 12:06:29 +0200

    6.3

---
+++ 6.3index.html/app.js
@@ -0,0 +1,5 @@
+function kleur_aanpassen() {
+    document.getElementById("h1").style.color = "red"; 
+}
+
+ 
--- P1-javascript/6.1index.html/6.3index.html/app.js
+++
@@ -1,3 +0,0 @@
-function kleur_aanpassen() {
-    document.getElementById("h1").style.color = "red"; }
-    console.log ("het werkt lol") 
