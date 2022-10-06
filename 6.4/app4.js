0bc77f0fdc48c782b498b5bd285dfa30e6d13f34
haville <9020580@student.zadkine.nl>
2022-10-06 15:08:36 +0200

    6.3

--- 6.4/app4.js
+++ 6.4/app4.js
@@ -5,3 +5,18 @@ function calculate(num, num2){
 }
 
 calculate(15, 2004);
+
+function multiplyByFive(num){
+let anwser = num * 5
+console.log(anwser);
+
+}
+multiplyByFive(800);
+
+
+function calculateMinutes(num){
+    let anwser = num / 60
+    console.log(anwser);
+}
+
+calculateMinutes(3600)
