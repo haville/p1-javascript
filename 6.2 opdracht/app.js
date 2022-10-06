0ec6955bec0a35bca55b62540fccee677689a2b9
haville <9020580@student.zadkine.nl>
2022-10-06 14:46:21 +0200

    java h6

---
+++ 6.2 opdracht/app.js
@@ -0,0 +1,3 @@
+function kleur_aanpassen() {
+document.getElementById("tit").style.color = "red"
+}
--- 6.3index.html/app.js
+++ 6.3index.html/app.js
@@ -2,4 +2,6 @@ function kleur_aanpassen() {
     document.getElementById("h1").style.color = "red"; 
 }
 
+
+
  
