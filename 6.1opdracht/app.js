a2bb9e7b92c1caf8396a0c14d4e5ee0d8e13ef30
haville <9020580@student.zadkine.nl>
2022-11-10 10:22:28 +0100

    6.1

---
+++ 6.1opdracht/app.js
@@ -0,0 +1,8 @@
+function kleur_aanpassen(){
+let title = document.getElementById("title");
+
+title.style.color = "blue";
+}
+
+kleur_aanpassen()
+
---
+++ 7.3/app.js
@@ -0,0 +1,11 @@
+let par= document.getElementById("paragraaf")
+
+let getal= 0
+
+function add (){
+    getal+= 1;
+}
+
+if(getal==10){
+getal=0;
+}
